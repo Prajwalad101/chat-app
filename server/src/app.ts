9758e933fb06dd59da8fce42513f0be0ec8a240f
Prajwal Adhikari <prajwalad101@gmail.com>
2022-06-06 07:23:27 +0545

    create http server and integrate socketio

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,1 +1,26 @@
-console.log('this is from app');
+import config from 'config';
+import express from 'express';
+import { createServer } from 'http';
+import { Server } from 'socket.io';
+import logger from './utils/logger';
+
+const port = config.get<number>('port');
+const host = config.get<string>('host');
+const corsOrigin = config.get<string>('corsOrigin');
+
+const app = express();
+
+const httpServer = createServer(app);
+
+const io = new Server(httpServer, {
+  cors: {
+    origin: corsOrigin,
+    credentials: true,
+  },
+});
+
+app.get('/', (_, res) => res.send(`Server is up`));
+
+httpServer.listen(port, host, () => {
+  logger.info('Server is listening');
+});
